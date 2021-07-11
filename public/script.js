902d1c77b3041ea10fb5c3e1d8975eeeac6c1d5c
Saransh khobragade <saransh@Saranshs-MacBook-Air.local>
2021-07-11 08:02:39 +0530

    test

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: 'localhost',
+  host: 'https://khobragade-video-chat.herokuapp.com/',
   port: 3001,
   path: '/peerjs'
 })
