c57c0aae2bf51a041bb3a4af017f71596312c641
Saransh khobragade <saransh@Saranshs-MacBook-Air.local>
2021-07-11 08:27:17 +0530

    done

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
 
 const { PeerServer } = require('peer');
 const peerServer = PeerServer({ port: 3001, path: '/peerjs' });
+
+const shortUuid = ()=>{
+  return Math.random().toString(36).slice(-6);
+}
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+  res.redirect(`/${shortUuid()}`)
 })
 
 app.get('/:room', (req, res) => {
